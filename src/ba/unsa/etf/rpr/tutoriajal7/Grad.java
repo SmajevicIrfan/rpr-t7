6e31010f1112983821ddb73114424d60e17d3e88
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-12-05 22:27:24 +0100

    Created class Grad

---
+++ src/ba/unsa/etf/rpr/tutoriajal7/Grad.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr.tutoriajal7;
+
+import java.io.Serializable;
+
+public class Grad implements Serializable {
+    String naziv;
+    int brojStanovnika;
+    int[] temperature;
+
+    public Grad() {
+        naziv = "";
+        brojStanovnika = 0;
+        temperature = new int[1000];
+    }
+
+    public Grad(String naziv, int brojStanovnika) {
+        this.naziv = naziv;
+        this.brojStanovnika = brojStanovnika;
+        temperature = new int[1000];
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public int[] getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int[] temperature) {
+        this.temperature = temperature;
+    }
+}
