c26e6853afb3c1552875ee7e8a743f7860c11220
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-12-07 17:24:08 +0100

    Added ability to load countries from xml

--- src/ba/unsa/etf/rpr/tutoriajal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutoriajal7/Tutorijal.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.tutoriajal7;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -43,7 +44,7 @@ public class Tutorijal {
         return result;
     }
 
-    /*private static UN ucitajXml(List<Grad> gradovi) {
+    private static UN ucitajXml(List<Grad> gradovi) {
         Document document = null;
         try {
             DocumentBuilder documentReader = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -53,23 +54,53 @@ public class Tutorijal {
         }
 
         NodeList countries = document.getDocumentElement().getChildNodes();
-        for (int i = )
-    }*/
+        List<Drzava> drzave = new ArrayList<>();
+        for (int i = 0; i < countries.getLength(); i++) {
+            Node child = countries.item(i);
+            if (child instanceof Element) {
+                Drzava newCountry = uzitajDrzavu((Element) child, gradovi);
+                if (newCountry != null) {
+                    drzave.add(newCountry);
+                }
+            }
+        }
+
+        UN response = new UN();
+        response.setDrzave(drzave);
+        return response;
+    }
+
+    private static Drzava uzitajDrzavu(Element element, List<Grad> context) {
+        Drzava response = new Drzava();
+        response.setBrojStanovnika(Integer.parseInt(element.getAttribute("stanovnika")));
+        response.setNaziv(element.getElementsByTagName("naziv").item(0).getTextContent());
+
+        Element area = (Element) element.getElementsByTagName("povrsina").item(0);
+        response.setPovrsina(Double.parseDouble(area.getTextContent()));
+        response.setJedinicaZaPovrsinu(area.getAttribute("jedinica"));
+
+        Element capitalCity = (Element) element.getElementsByTagName("glavnigrad").item(0);
+        Grad capital = new Grad();
+        capital.setNaziv(capitalCity.getElementsByTagName("naziv").item(0).getTextContent());
+        int capitalCityIndex = context.indexOf(capital);
+        if (capitalCityIndex == -1) {
+            return null;
+        }
+
+        response.setGlavniGrad(context.get(capitalCityIndex));
+        return response;
+    }
 
     public static void main(String[] args) {
         try {
             List<Grad> cities = ucitajGradove();
+            UN un = ucitajXml(cities);
 
-            for (Grad city : cities) {
-                System.out.print(city.getNaziv() + ": ");
-                for (double temperature : city.getTemperature()) {
-                    System.out.print(temperature + " ");
-                }
-
-                System.out.println();
+            for (Drzava country : un.getDrzave()) {
+                System.out.println(country.getNaziv());
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+            System.out.println("Datoteka ne postoji ili se ne može otvoriti");
         }
     }
 }
